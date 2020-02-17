9deef8eff5a4606477d009a39bd07ed6be420d47
YevgenyMoskalov <lesnoy022@gmail.com>
2020-02-18 01:39:58 +0200

    herocu create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,7 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  # rescue_from CanCan::AccessDenied do
-  #   redirect_to '/403.html'
-  # end
-
-  rescue_from ActiveRecord::RecordNotFound do
-    redirect_to '/404.html'
-  end
-
   def configure_permitted_parameters
-    # devise_parameter_sanitizer.permit(:sign_up) do |client|
-    #   client.permit(client_attr_attributes: %i[first_name last_name gender birthday])
-    # end
     devise_parameter_sanitizer.permit(:sign_up, keys: [client_attr_attributes: %i[first_name last_name gender birthday]])
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'account/index'
-  devise_for :clients, controllers: { registrations: 'clients/registrations' }
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
 
+  get 'account/index'
+  devise_for :clients, controllers: { sessions: 'client/sessions', registrations: 'clients/registrations' }
+  devise_for :users,   controllers: { sessions: 'users/sessions' }
+  devise_for :manager, controllers: { sessions: 'manager/sessions', registrations: 'manager/registrations' }
   root 'account#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
