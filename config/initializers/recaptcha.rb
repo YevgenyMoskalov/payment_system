032b6ed7381793f649af69941cf51c0732a5525f
YevgenyMoskalov <lesnoy022@gmail.com>
2020-02-20 15:13:16 +0200

    add ENV.fetch to config heroku

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -2,5 +2,6 @@ Recaptcha.configure do |config|
   config.site_key   = ENV_RECAPTCHA_PUBLIC_KEY
   config.secret_key = ENV_RECAPTCHA_PRIVATE_KEY
   config.proxy = '127.0.0.1'
-
+  ENV.fetch(ENV_RECAPTCHA_PRIVATE_KEY)
+  ENV.fetch(ENV_RECAPTCHA_PRIVATE_KEY)
 end
