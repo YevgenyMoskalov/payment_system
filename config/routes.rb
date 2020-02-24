26b29a4963cc7b21da9efe844c2d27ed005b11dd
YevgenyMoskalov <lesnoy022@gmail.com>
2020-02-24 10:32:48 +0200

    remove omniauth_callbacks_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  match "/404", :to => "errors#not_found", :via => :all
-  match "/500", :to => "errors#internal_server_error", :via => :all
+  match '/404', to: 'errors#not_found', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
 
   get 'account/index'
+  # :omniauth_callbacks => "users/omniauth_callbacks"
+
   devise_for :clients, controllers: { sessions: 'client/sessions', registrations: 'clients/registrations' }
   devise_for :users,   controllers: { sessions: 'users/sessions' }
   devise_for :manager, controllers: { sessions: 'manager/sessions', registrations: 'manager/registrations' }
