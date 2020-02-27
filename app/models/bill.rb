c4fa331657b3030e22b3e7064cc05f3580959b0e
YevgenyMoskalov <lesnoy022@gmail.com>
2020-02-27 17:48:07 +0200

    simple commit

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -3,7 +3,7 @@ class Bill < ApplicationRecord
   has_one :bill_request
   has_many :bill_requests
   has_many :activities
-  # belongs_to :client_attr
+  belongs_to :client
 
   #validates :amount, :bills_type, :percent, :amount_limit, :regular_replenishment_amount, presence: true
   # validates :percent, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }, presence: true
